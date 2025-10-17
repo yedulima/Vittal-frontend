0438262e1996187425029635383734389047c26e
yedulima <eduardolimacmj2017@gmail.com>
2025-10-17 13:32:11 -0300

    feat: created notification component and added content to notification screen

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,20 +1,38 @@
 import { StyleSheet } from 'react-native';
 
-import ThemedText from '@/components/ThemedText';
+import Notification from '@/components/Notification';
 import ThemedView from '@/components/ThemedView';
 
 export default function NotificationsScreen() {
-    return (
-        <ThemedView style={styles.container}>
-            <ThemedText text="Notifications screen" type='subtitle' />
-        </ThemedView>
-    );
+	return (
+		<ThemedView style={styles.container}>
+			<Notification
+				description="Ana não tomou o remédio da noite."
+				type="Alerta de Medicamento"
+				isView={true}
+				date="15/07/2024 - 10:30"
+				onPress={() => {}}
+			/>
+			<Notification
+				description="Acionado por Gabriela Freitas."
+				type="Alerta de SOS"
+				isView={false}
+				date="15/07/2024 - 10:30"
+				onPress={() => {}}
+			/>
+			<Notification
+				description="Já baixou os novos updates?"
+				isView={false}
+				date="15/07/2024 - 10:30"
+				onPress={() => {}}
+			/>
+		</ThemedView>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+	container: {
+		flex: 1,
+		padding: 28,
+	},
 });
---
+++ components/Notification.tsx
@@ -0,0 +1,142 @@
+import { useThemeColor } from '@/hooks/useThemeColor';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
+
+import ThemedText from './ThemedText';
+
+type NotificationTypeName = 'Alerta de SOS' | 'Nova notificação' | 'Alerta de Medicamento';
+
+interface NotificationTitleProps {
+	title?: string;
+	type: NotificationTypeName;
+}
+
+export type NotificationProps = TouchableOpacityProps & {
+	title?: string;
+	type?: NotificationTypeName;
+	description: string;
+	isView: boolean;
+	date: string;
+	onPress: () => void;
+};
+
+export default function Notification({
+	title,
+	type = 'Nova notificação',
+	description,
+	isView,
+	date,
+	onPress,
+	style,
+	...rest
+}: NotificationProps) {
+	const button = useThemeColor('button');
+	const secondaryTextColor = useThemeColor('secondaryTextColor');
+	const card = useThemeColor('card');
+
+	return (
+		<TouchableOpacity
+			onPress={onPress}
+			style={[
+				{ borderBottomColor: button, backgroundColor: isView ? 'transparent' : card },
+				styles.container,
+				style,
+			]}
+			activeOpacity={0.7}
+			{...rest}
+		>
+			<View style={styles.notificationInfoContainer}>
+				<NotificationIcon type={type} />
+				<View style={styles.textContainer}>
+					<View style={styles.titleContainer}>
+						<NotificationTitle title={title} type={type} />
+						<ThemedText text={date} style={[{ color: secondaryTextColor }, styles.dateText]} />
+					</View>
+					<View style={styles.descriptionContainer}>
+						<ThemedText
+							text={description}
+							style={[{ color: secondaryTextColor }, styles.descriptionText]}
+						/>
+					</View>
+				</View>
+			</View>
+		</TouchableOpacity>
+	);
+}
+
+const NotificationTitle = ({ title, type }: NotificationTitleProps) => {
+	const textColor = useThemeColor('textColor');
+	const notificationColor = useThemeColor('notification');
+
+	if (type === 'Alerta de SOS') {
+		return <ThemedText text={title ? title : type} style={[{ color: notificationColor }, styles.title]} />;
+	} else if (type === 'Alerta de Medicamento') {
+		return <ThemedText text={title ? title : type} style={[{ color: '#dfa820' }, styles.title]} />;
+	} else {
+		return <ThemedText text={title ? title : type} style={[{ color: textColor }, styles.title]} />;
+	}
+};
+
+const NotificationIcon = ({ type }: { type: NotificationTypeName }) => {
+	const notificationColor = useThemeColor('notification');
+	const secondaryTextColor = useThemeColor('secondaryTextColor');
+
+	if (type === 'Alerta de SOS') {
+		return <Ionicons name="alert-circle-outline" size={42} color={notificationColor} />;
+	} else if (type === 'Alerta de Medicamento') {
+		return <Ionicons name="medical" size={41} color={'#dfa820'} />;
+	} else {
+		return <Ionicons name="notifications-circle-outline" size={43} color={secondaryTextColor} />;
+	}
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		width: '100%',
+		height: 80,
+		paddingHorizontal: 7,
+		paddingBottom: 10,
+		borderBottomWidth: 2,
+		marginBottom: 10,
+		borderRadius: 5,
+	},
+	notificationInfoContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		gap: 12,
+	},
+	textContainer: {
+		flex: 1,
+		alignItems: 'flex-start',
+		justifyContent: 'center',
+	},
+	titleContainer: {
+		width: '97%',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	},
+	descriptionContainer: {
+		width: '97%',
+		maxHeight: 40,
+	},
+	title: {
+		fontSize: 16,
+		fontWeight: '500',
+	},
+	dateText: {
+		fontSize: 11.5,
+	},
+	descriptionText: {
+		fontSize: 13,
+	},
+	status: {
+		fontSize: 12,
+		fontWeight: 'light',
+	},
+});
