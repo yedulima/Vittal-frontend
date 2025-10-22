2c18794cc94439e730472ce80ffe0d6c450df260
yedulima <eduardolimacmj2017@gmail.com>
2025-10-22 03:01:55 -0300

    fix: background color not themed appearing during screen change animation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,65 +2,73 @@ import { useThemeColor } from '@/hooks/useThemeColor';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from 'expo-router';
 
+import ThemedView from '@/components/ThemedView';
+
 export default function TabLayout() {
 	const { primary, primaryText, header } = useThemeColor();
 
 	return (
-		<Tabs
-			screenOptions={{
-				tabBarActiveTintColor: primary,
-				headerStyle: {
-					backgroundColor: header,
-				},
-				headerShadowVisible: false,
-				headerTintColor: primaryText,
-				tabBarStyle: {
-					backgroundColor: header,
-				},
-				headerTitleAlign: 'center',
-				animation: 'shift',
-			}}
-		>
-			<Tabs.Screen
-				name="index"
-				options={{
-					title: 'Home',
-					headerShown: false,
-					tabBarIcon: ({ color, focused }) => (
-						<Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
-					),
+		<ThemedView style={{ flex: 1 }}>
+			<Tabs
+				screenOptions={{
+					tabBarActiveTintColor: primary,
+					headerStyle: {
+						backgroundColor: header,
+					},
+					headerShadowVisible: false,
+					headerTintColor: primaryText,
+					tabBarStyle: {
+						backgroundColor: header,
+					},
+					headerTitleAlign: 'center',
+					animation: 'shift',
 				}}
-			/>
-			<Tabs.Screen
-				name="contacts"
-				options={{
-					title: 'Contatos',
-					headerShown: false,
-					tabBarIcon: ({ color, focused }) => (
-						<Ionicons name={focused ? 'people' : 'people-outline'} color={color} size={24} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="notifications"
-				options={{
-					title: 'Notificações',
-					headerShown: false,
-					tabBarIcon: ({ color, focused }) => (
-						<Ionicons name={focused ? 'notifications' : 'notifications-outline'} color={color} size={24} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="configurations"
-				options={{
-					title: 'Configurações',
-					headerShown: false,
-					tabBarIcon: ({ color, focused }) => (
-						<Ionicons name={focused ? 'cog' : 'cog-outline'} color={color} size={24} />
-					),
-				}}
-			/>
-		</Tabs>
+			>
+				<Tabs.Screen
+					name="index"
+					options={{
+						title: 'Home',
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="contacts"
+					options={{
+						title: 'Contatos',
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<Ionicons name={focused ? 'people' : 'people-outline'} color={color} size={24} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="notifications"
+					options={{
+						title: 'Notificações',
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<Ionicons
+								name={focused ? 'notifications' : 'notifications-outline'}
+								color={color}
+								size={24}
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="configurations"
+					options={{
+						title: 'Configurações',
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<Ionicons name={focused ? 'cog' : 'cog-outline'} color={color} size={24} />
+						),
+					}}
+				/>
+			</Tabs>
+		</ThemedView>
 	);
 }
