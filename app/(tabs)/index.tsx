b265f72ebd077fb9f85c79eaeb361406d13ef0cb
yedulima <eduardolimacmj2017@gmail.com>
2025-11-19 16:28:08 -0300

    feat: added overviewlist role verification

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { useAuthContext } from '@/contexts/AuthContext';
 import { useThemeContext } from '@/contexts/ThemeContext';
 import { homeStyles } from '@/styles/screens/HomeStyles';
 import { NOTIFICATIONS, OVERVIEW } from '@/utils/data';
@@ -8,13 +9,14 @@ import LastNotificationsList from '@/components/LastNotificationsList';
 import OverViewList from '@/components/OverViewList';
 
 export default function HomeScreen() {
+	const { role } = useAuthContext();
 	const { colors } = useThemeContext();
 	const styles = homeStyles(colors!);
 
 	return (
 		<SafeAreaView style={styles.container}>
 			<Header />
-			<OverViewList data={OVERVIEW} />
+			{role === 'cuidador' && <OverViewList data={OVERVIEW} />}
 			<LastNotificationsList data={NOTIFICATIONS} />
 		</SafeAreaView>
 	);
